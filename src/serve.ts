5492c0b2de46782b491f4a9fe5f50c3d1eccbf13
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2024-09-16 08:31:42 -0600

    Creando routing para proyectos

--- src/serve.ts
+++ src/serve.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
+import projectRoutes from './routes/projectRoutes'
 
 
 dotenv.config()
@@ -9,6 +10,7 @@ connectDB()
 
 const app = express()
 
-
+// Routes
+app.use('/api/projects', projectRoutes)
 
 export default app
