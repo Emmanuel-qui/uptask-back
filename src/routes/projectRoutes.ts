59af0c2aa6545972a6e201addd442a094601eb11
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2024-09-16 13:13:07 -0600

    Agregue validacion en el servidor para la creacion de proyectos

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
 
-router.post('/', ProjectController.createProject)
+router.post('/', 
+    body('projectName').notEmpty().withMessage('El campo nombre del proyecto es requerido.'),
+    body('clientName').notEmpty().withMessage('El campo nombre del cliente es requerido.'),
+    body('description').notEmpty().withMessage('El campo descripcion es requerido.'),
+    handleInputErrors,
+    ProjectController.createProject
+)
+
 router.get('/', ProjectController.getAllProjects)
 
 
