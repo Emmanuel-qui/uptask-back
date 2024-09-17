d96027f1e81e901a731697d0abe2928dc8836a2d
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2024-09-17 08:53:59 -0600

    Mejorando el codigo con PromiseAllSettled

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from 'express'
 import { HttpStatus } from '../utils/HttpStatus'
-import Project from '../models/Project'
 import Task from '../models/Task'
 
 export class TaskController {
@@ -11,9 +10,8 @@ export class TaskController {
             const task = new Task(req.body)
             task.project = req.project.id
             req.project.tasks.push(task.id)
-            await task.save()
-            await req.project.save()
-
+            await Promise.allSettled([task.save(), req.project.save()])
+            
             res.status(HttpStatus.CREATED).json({message: 'Tarea creada exitosamente'})
 
         } catch (error) {
